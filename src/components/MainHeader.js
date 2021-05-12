c7f0a5ba28a0b32afe1eca5e24f72f018efd9956
Darshan Satya <darshans1989@gmail.com>
2021-05-11 20:13:29 -0400

    Navlinks

---
+++ src/components/MainHeader.js
@@ -0,0 +1,23 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "./MainHeader.module.css";
+
+const MainHeader = () => {
+  return (
+    <header className={styles.header}>
+      <ul>
+        <li>
+          <NavLink activeClassName={styles.active} to="/welcome">
+            Welcome
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName={styles.active} to="/products">
+            Products
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default MainHeader;
