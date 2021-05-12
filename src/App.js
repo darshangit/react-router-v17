4c98a6c5d9c9db43adc914f8a2bfa6c973ff7354
Darshan Satya <darshans1989@gmail.com>
2021-05-11 20:32:31 -0400

    basic routing with swicth and exact

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import MainHeader from "./components/MainHeader";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductsDetails";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <MainHeader/>
+      <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
